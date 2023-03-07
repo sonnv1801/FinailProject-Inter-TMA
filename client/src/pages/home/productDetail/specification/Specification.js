99bc82fa273d4cbe25220ce3b5aedaac66210e13
philipkaka2001 <philipkaka2001@gmail.com>
2023-03-07 15:11:01 +0700

    philip-commit

--- client/src/pages/home/productDetail/specification/Specification.js
+++ client/src/pages/home/productDetail/specification/Specification.js
@@ -1,47 +1,56 @@
 import React from 'react';
 import './style.css';
-const ts = [
-  {
-    id: 1,
-    title: 'Bộ nhớ Trong',
-    name: '128GB',
-    color: 'red',
-  },
-  {
-    id: 2,
-    title: 'Bộ nhớ Trong',
-    name: '128GB',
-    color: 'gray',
-  },
-  {
-    id: 3,
-    title: 'Bộ nhớ Trong',
-    name: '128GB',
-    color: 'black',
-  },
-  {
-    id: 4,
-    title: 'Bộ nhớ Trong',
-    name: '128GB',
-    color: 'white',
-  },
-];
+
 const Specification = () => {
   return (
     <div className="specifications">
       <b>Thông số kỹ thuật</b>
+      <table class="table table-hover">
+        <tbody>
+          <tr>
+            <td>Màu sắc</td>
+            <td>ABC</td>
+            <td></td>
+          </tr>
+          <tr>
+            <td>Màn hình</td>
+            <td>IPS LCD6.1"Liquid Retina</td>
+          </tr>
+          <tr>
+            <td>Hệ điều hành</td>
+            <td>iOS 15</td>
+          </tr>
+          <tr>
+            <td>Camera sau</td>
+            <td>2 camera 12 MP</td>
+          </tr>
+          <tr>
+            <td>Camera trước</td>
+            <td> 12 MP</td>
+          </tr>
+          <tr>
+            <td>Chip</td>
+            <td>Apple A13 Bionic</td>
+          </tr>
 
-      <div className="sub-specifications">
-        {ts.map((item, index) => (
-          <div className="sub-body-specifications" key={index}>
-            <ul>
-              <li></li>
-              <li></li>
-              <li></li>
-            </ul>
-          </div>
-        ))}
-      </div>
+          <tr>
+            <td>RAM</td>
+            <td>4 GB</td>
+          </tr>
+          <tr>
+            <td>Dung lượng lưu trữ</td>
+            <td>128 GB</td>
+          </tr>
+          <tr>
+            <td>SIM</td>
+            <td>1 Nano SIM & 1 eSIMHỗ trợ 4G</td>
+          </tr>
+          <tr>
+            <td>Pin, Sạc</td>
+            <td>3110mAh 18W</td>
+          </tr>
+        </tbody>
+      </table>
     </div>
   );
 };
